902a50ae4a617a9a83391c4dd60048e2243abab3
kekucode <lyzerdong03@gmail.com>
2023-08-19 23:17:30 +0200

    "fixe text color"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   }
   return (
     <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">The Best <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">AI</span> Tool</h1>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">The Best <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">AI</span> Tool</h1>
       <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl ">Create all your contents wAIsely using AI 10x faster.</p>
       <Link href="/signin" className="flex items-center gap-2">
         <p className='mb-6 rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block'>
